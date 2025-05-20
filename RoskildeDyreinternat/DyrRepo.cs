975802e36d04ea74e592b17fcb9642ebe39e019e
Julia Jolanta Pawlaczyk <jup005@edu.zealand.dk>
2025-05-20 13:35:51 +0200

    if

--- RoskildeDyreinternat/DyrRepo.cs
+++ RoskildeDyreinternat/DyrRepo.cs
@@ -15,38 +15,77 @@ namespace RoskildeDyreinternat
 
         // HundeListe
         public bool AddHund(Hund hund)
-            {
+        {
             if (hund != null && !HundeListe.Contains(hund))
-
-                {
-                    this.HundeListe.Add(hund);
-                    return true;
-                }
-                return false;
+            {
+                this.HundeListe.Add(hund);
+                return true;
             }
+            else
+            {
+                // Eksempel: du kan tilføje en logbesked her
+                Console.WriteLine("Hunden findes allerede.");
+                return false;
+
+            }
+        }
+            
 
             public List<Hund> GetAllHunde()
             {
                 return HundeListe;
             }
-  
-     
+
+
         // KatteListe
-            public bool AddKat(Kat kat)
+        public bool AddKat(Kat _kat)
+        {
+
+            foreach (Kat kat in KatteListe)
             {
-                if (kat != null && !KatteListe.Contains(kat))
-            {
-                    this.KatteListe.Add(kat);
+                //Tjekker om den nyoprettede kat, ikke har samme chipnummer som en kat på listen.
+                if (_kat != null && kat.chipnummer==_kat.chipnummer)
+                {
+                    KatteListe.Add(_kat); // gemmer hvilke katte der blev tilføjet
                     return true;
                 }
-                return false;
             }
 
-            public List<Kat> GetAllKatte()
-            {
-                return KatteListe;
-            }
-        
+            //for-loop: Tjekker om den nyoprettede kat, ikke har samme chipnummer som en kat på listen.
+            //for (int i = 0; i < KatteListe.Count; i++)
+            //{
+            //    if (!(KatteListe[i].chipnummer == _kat.chipnummer))
+            //    {
+            //        KatteListe.Add(_kat);
+            //        return true;
+            //    }
+            //}
+
+
+            return false; // returnerer listen over katte, der blev tilføjet
+        }
+
+        //While-loop: Tjekker om den nyoprettede kat, ikke har samme chipnummer som en kat på listen.
+        //public bool AddKat(Kat _kat)
+        //{
+        //    if (_kat == null)
+        //        return false;
+
+        //    int i = 0;
+        //    while (i < KatteListe.Count)
+        //    {
+        //        if (KatteListe[i].chipnummer == _kat.chipnummer)
+        //        {
+        //            return false; // Dublet fundet – tilføj ikke
+        //        }
+        //        i++;
+        //    }
+
+        //    KatteListe.Add(_kat); // Ingen dublet fundet – tilføj katten
+        //    return true;
+        //}
+
+
     }
 
 }
