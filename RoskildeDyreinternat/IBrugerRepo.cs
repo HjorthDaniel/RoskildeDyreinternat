513ccc749fc0cb9e3a55d288933d22d950a3d58d
Daniel Hjorth Bloch Nielsen <dan003@edu.zealand.dk>
2025-05-23 09:58:40 +0200

    d

--- RoskildeDyreinternat/IBrugerRepo.cs
+++ RoskildeDyreinternat/IBrugerRepo.cs
@@ -8,5 +8,12 @@ namespace RoskildeDyreinternat
 {
     public interface IBrugerRepo
     {
+        void OpretKunde(int id, string navn, string email, string telefon, string adresse, string rolle, string køn, int alder)
+   void OpretMedarbejder(int id, string navn, string email, string telefon, string adresse, string rolle, string stilling, int antalarbejdstimer)
+   void VisBrugerInfo(int id)
+   void OpdaterKundeInfo(int id, string navn, string email, string telefon, string adresse)
+   void SletKunde(int id)
+   void VisBrugerRolle(int id)
+   void VisBrugerInfo(int id)
     }
 }
